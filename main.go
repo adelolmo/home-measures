7601ab9c585afae833a01d23db215da0155890fa
andoni <andoni.delolmo@gmail.com>
2017-05-20 22:06:07 +0200

    first working version.

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"net/http"
+	"database/sql"
+	"encoding/json"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/gorilla/mux"
+)
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "./foo.db")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You connected to your database.")
+
+	sqlStmt := `
+	create table if not exists measures (temperature integer not null , created_at TEXT not null);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		fmt.Errorf("%q: %s\n", err, sqlStmt)
+	}
+}
+
+type Measures struct {
+	Measures []Measure `json:"measures"`
+}
+
+type Measure struct {
+	Temperature int `json:"temperature"`
+	CreatedAt   string `json:"createdAt"`
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/measures", getMeasures).Methods("GET")
+	router.HandleFunc("/measures", addMeasure).Methods("POST")
+
+	p := fmt.Sprintf("%s:%s", "localhost", "8080")
+	fmt.Printf("Listening on %s...\n", p)
+	log.Fatal(http.ListenAndServe(p, router))
+}
+
+func getMeasures(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("GET /measures")
+
+	rows, err := db.Query("SELECT temperature, created_at FROM measures order by created_at desc")
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	defer rows.Close()
+
+	measures := make([]Measure, 0)
+
+	for rows.Next() {
+		m := Measure{}
+		err := rows.Scan(&m.Temperature, &m.CreatedAt)
+		if err != nil {
+			http.Error(w, "{\"message\":\"Not found\",\"code\":404}", 404)
+			return
+		}
+		measures = append(measures,
+			Measure{Temperature:m.Temperature,
+				CreatedAt:m.CreatedAt})
+
+	}
+	if err = rows.Err(); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	json.NewEncoder(w).Encode(Measures{Measures:measures})
+}
+
+func addMeasure(w http.ResponseWriter, req *http.Request) {
+	var m Measure;
+	json.NewDecoder(req.Body).Decode(&m);
+
+	stmt, err := db.Prepare("INSERT INTO measures(temperature, created_at) values(?, ?)")
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	res, err := stmt.Exec(m.Temperature, time.Now().Format("2006-01-02T15:04:05.511Z"))
+	_, err = res.LastInsertId()
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+}
